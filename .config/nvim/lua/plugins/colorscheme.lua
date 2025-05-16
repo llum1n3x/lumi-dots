25cb752d5a0a02f968c557a06b6eabe32daba323
Luminex <167656454+llum1n3x@users.noreply.github.com>
2025-05-16 23:45:51 +0700

    Add files via upload

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  { "chriskempson/base16-vim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vague",
+    },
+  },
+}
---
+++ .config/nvim/lua/plugins/rustacean.lua
@@ -0,0 +1,6 @@
+return {
+  "mrcjkb/rustaceanvim",
+  version = "^6", -- Recommended
+  build = "rustup component add rust-analyzer",
+  ft = { "rust" },
+}
